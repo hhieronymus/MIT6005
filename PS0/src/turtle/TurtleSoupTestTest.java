450af0bd247babef9afe5629bc94ffe469556fdc
H. Hieronymus <hhieronymus.code@gmail.com>
2018-08-12 19:18:12 -0400

    Initial revision

---
+++ PS0/src/turtle/TurtleSoupTestTest.java
@@ -0,0 +1,14 @@
+package turtle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TurtleSoupTestTest extends TurtleSoupTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
